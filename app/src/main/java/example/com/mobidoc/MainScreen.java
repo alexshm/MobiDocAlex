e21546013f93a5e0ce07af0e7ae933f00f6c9322
sheinmos <m.sheinberger@gmail.com>
2015-02-16 22:00:11 +0200

    update the GUI for supporting start time + reminders

--- app/src/main/java/example/com/mobidoc/MainScreen.java
+++ app/src/main/java/example/com/mobidoc/MainScreen.java
@@ -64,6 +64,17 @@ public class MainScreen extends Activity {
         // Apply the adapter to the spinner
         spinner.setAdapter(adapter);
         //===============
+        // spiner for the remainder combo box
+        //===================
+        Spinner reminder_spinner = (Spinner) findViewById(R.id.spinner2);
+        // Create an ArrayAdapter using the string array and a default spinner layout
+        ArrayAdapter<CharSequence> adapterReminder = ArrayAdapter.createFromResource(this,
+                R.array.reminder_elements, android.R.layout.simple_spinner_item);
+        // Specify the layout to use when the list of choices appears
+        adapterReminder.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        // Apply the adapter to the spinner
+        reminder_spinner.setAdapter(adapterReminder);
+        //===============
 
         startbtn.setOnClickListener(new View.OnClickListener() {
                 @Override
