54b2b147c5873a2bdc980d0a37132d44cd7c6acf
alexshm <cooL88**>
2015-03-12 15:36:02 +0200

    sope chnage made to msgReciver

--- app/src/main/java/example/com/mobidoc/MsgRecieverService.java
+++ app/src/main/java/example/com/mobidoc/MsgRecieverService.java
@@ -2,13 +2,11 @@ package example.com.mobidoc;
 
 import android.app.Service;
 import android.content.Intent;
-import android.os.Bundle;
 import android.os.Handler;
 import android.os.IBinder;
 import android.os.Message;
 import android.os.Messenger;
 
-import java.util.Random;
 
 
 public class MsgRecieverService extends Service {
