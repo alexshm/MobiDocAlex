eb0d36bc144425dc2b6752c7b8ddc6ffd71628f5
sheinmos <m.sheinberger@gmail.com>
2015-03-29 23:45:01 +0300

    add action to projecction throught the script

--- app/src/main/java/projections/ScriptingLayer/JsScriptExecutor.java
+++ app/src/main/java/projections/ScriptingLayer/JsScriptExecutor.java
@@ -26,7 +26,10 @@ import java.util.Date;
  * Created by Moshe on 3/18/2015.
  */
 
-
+ interface cont
+{
+    
+}
 
 public class JsScriptExecutor {
 //
@@ -77,10 +80,65 @@ try {
     {
         return new CyclicProjectionAbstract(name,c,"08:00");
     }
-    public   void runScript(String scriptToRun) {
 
 
-      //  scriptToRun=getScriptFromServer();
+    public void continueTest()
+    {
+
+        /*
+            we will create the obj projection not in script
+            for the recommendation/qeustion we will make
+            set concepts for answers(or set answerConcepts)
+            then we will setOnconceptRecive(string concept,composite action)
+
+            the declaration of the composite action will done erallier and we pass just
+            the name(or id) and search in the map /globals vars
+
+            then we will do something like p.action.setOncecept..()
+            and inside the method that will implemented in the projection class
+            we search the name in the composite actions map and connect to it.
+
+          1)  in the java scriipt i declare an object - actionSequance()-emotye constractor
+            actionsequance: name
+                           : list of actionsToPreform need to be also actionsToPreform() -constractor
+                                      each action like this has:
+                                      {
+                                            name(identifier)
+                                             concept
+                                             type(measure/callback/quetion/notifiaction....)
+                                             txt of msg
+
+                                          }
+                           :order (sequncial/parralel)
+           2)  we will have javascript function -declareActionsequance(name)
+                in this function we call to a function in ht projection class that init a new CompositeAction
+                and insert to the map of composite.
+            3) we will set a function to the javascript actionSequance obj that will add a  new
+            actionsToPreform() obj will all the needed params.
+            4) than again in the javascript function -declareActionsequance(name):-
+                we will iterate for all the actions in the list and build a coresponding Action object
+                andd add to the composite action we declare before.
+
+
+
+               5) need to change the monitoring/cyclic projection for triggring the action by name
+
+           --    6) add implementation to thr projection addAction(name..params)/(name,Action)
+
+
+
+
+         */
+
+    }
+
+    public projection buildProjection()
+    {
+        return null;
+    }
+    public   void runScript(String scriptToRun,projection p) {
+
+       scriptToRun=getScriptFromServer();
        Context context = Context.enter();
         context.setOptimizationLevel(-1);
        try
@@ -99,8 +157,7 @@ try {
             scope.put("test",scope,testclass);
             Class<DataItem> dataitemClass = (Class<DataItem>)classloader.loadClass("projections.DataItem");
             scope.put("dataitemClass",scope,dataitemClass);
-
-            TypeId<DataItem> a = TypeId.get(dataitemClass);
+            scope.put("proj",scope,p);
 
             scope.put("builder", scope, new ProjectionBuilder(c));
 
@@ -115,18 +172,21 @@ try {
                 //   "java.lang.System.out.println('the type is   ' + temp.getProjectionName());"+
 
 
-                    "var newinstance=new dataitemClass('11' ,'22',today);";
+                    "var newinstance=new actionsToPreform('11' ,'22','22','test');" +
 
-           //working "var newinstance=dataitemClass(con ,eee,today);";
-                   // "var createdata= function (concept,val,date) {var newinstance=dataitemClass.newInstance();";
-           // "var methodbuildProj = DataItem.getMethod(\"buildProj\", [java.lang.String]);" +
-                 //   "var proj = function (name) {return methodbuildProj.invoke(null, name);};";
-                           // "var proj = function (name) {return methodbuildProj.invoke(null, name);};";
-                 //   "var s = item('12','111',today);";
+                    "var comp=new actionSequance();"+
+                   //"java.lang.System.out.println('the size is : '+comp.actionList.length);"+
+                    //"comp.actionList.push(newinstance);"+
 
-            // Execute the script
+                    "comp.addAction(newinstance);"+
+                     "java.lang.System.out.println('the size is : '+comp.actionList.length);"+
+                    "comp.printElements();";
 
-            Object obj = context.evaluateString( scope, script, "TestScript", 1, null );
+
+
+         // Execute the script
+
+            Object obj = context.evaluateString( scope, scriptToRun+script, "TestScript", 1, null );
             System.out.println( "Object: " + obj );
 
             // Cast the result to a string
@@ -149,16 +209,50 @@ try {
 }
 
     //receive the Script from the server with Http request
-    private  String getScriptFromServer(String r) {
+    private  String getScriptFromServer() {
 
 
-           String  projectionObj = "var projectionToBuild = java.type(\"projections.projection\");";
 
-            String script = "function build(type,pname,cont) " +
-                    "{if (type==\"monitor\")" +
-                    "{return  projections.CyclicProjectionAbstract(null,null,null);}}";
+            String script =
+                    "function actionsToPreform(name,type,concept,txt)"
+                    +"{this.name=name;"+
+                      "this.type=type;this.concept=concept;this.txt=txt;};"+
 
-            return DataItem;
+                    "function actionSequance(name) {" +
+                            "this.name=name;};"+
+
+
+                    " actionSequance.prototype.actionList=[];"+
+                    "actionSequance.prototype.addAction=function(action){\n" +
+                            "actionSequance.prototype.__defineSetter__('addaction', function(action)\n\t" +
+                                     "{ actionSequance.prototype.actionList.push(action);});\n" +
+                                      "this.addaction=action;\n"+
+                                      // "java.lang.System.out.println('the size is : '+actionList);" +
+                            "};"+
+
+                    " actionSequance.prototype.printElements=function(){" +
+                            "for(var i=0;i< actionSequance.prototype.actionList.length;i++)\n" +
+                            "{\n" +
+                            "java.lang.System.out.println('the actionName is :  ' + actionSequance.prototype.actionList[i].name);" +
+                            "}"+
+                            "};" ;
+
+
+                            /*
+                       "this.printElements=function(){" +
+                            "for(var i=0;i< this.actionList.length;i++)\n" +
+                            "{\n" +
+                            "java.lang.System.out.println('the actionName is :  ' + actionList[i].name);" +
+                            "}"+
+                           "};" +
+                           "};\n\n" ;
+                           */
+
+
+
+
+
+            return script;
 
 
     }
