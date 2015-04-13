921e28095df6b127abf148b807cd8b8a788ff84f
israelru <israel.rukshin@gmail.com>
2015-04-13 22:29:42 +0300

    parse login

--- app/src/main/java/example/com/mobidoc/Screens/webComScreen.java
+++ app/src/main/java/example/com/mobidoc/Screens/webComScreen.java
@@ -57,7 +57,7 @@ public class webComScreen extends Activity {
         String concept =  "5085AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA";
         String personID = "7014135e-7575-41a8-8832-4fb0c3b2ca5f";
         String dateTime = "2010-03-23T00:00:00.000+0200";
-        ans.setText(openMrsApi.getSession());
+        ans.setText(openMrsApi.getSession()+"");
 //        ans.setText(openMrsApi.postObs(personID,88,dateTime,concept));
       //  ans.setText(openMrsApi.getPerson("israel"));
 
