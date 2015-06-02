94f27bd73a0374228f4cf7664047ebf1ee5220cf
sheinmos <m.sheinberger@gmail.com>
2015-06-03 01:13:17 +0300

    fix measures screen

--- app/src/main/java/example/com/mobidoc/Screens/MeasuresScreen.java
+++ app/src/main/java/example/com/mobidoc/Screens/MeasuresScreen.java
@@ -1,169 +1,164 @@
 package example.com.mobidoc.Screens;
 
-
 import android.annotation.TargetApi;
-import android.app.ActionBar;
 import android.app.Activity;
-import android.app.FragmentTransaction;
+import android.app.Dialog;
+import android.app.ProgressDialog;
 import android.graphics.Color;
 import android.graphics.drawable.Drawable;
 import android.os.AsyncTask;
 import android.os.Build;
 import android.os.Bundle;
-import android.app.FragmentTransaction;
-import android.support.v4.app.FragmentActivity;
-import android.support.v4.view.ViewPager;
-
-
-import android.support.v4.app.FragmentPagerAdapter;
-
 import android.util.Log;
 import android.view.Gravity;
 import android.widget.HorizontalScrollView;
-import android.widget.ScrollView;
-import android.widget.TableLayout;
-import android.support.v4.app.FragmentManager;
-import android.support.v4.app.FragmentPagerAdapter;
-import android.support.v4.view.ViewPager;
-import android.view.Gravity;
-import android.widget.HorizontalScrollView;
+import android.widget.LinearLayout;
 import android.widget.ScrollView;
 import android.widget.TableLayout;
 import android.widget.TableRow;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.concurrent.ExecutionException;
-import java.util.concurrent.Executor;
 
 import example.com.mobidoc.CommunicationLayer.OpenMrsApi;
-import example.com.mobidoc.CommunicationLayer.ServicesToBeDSS.PicardCommunicationLayer;
 import example.com.mobidoc.ConfigReader;
 import example.com.mobidoc.R;
-
 
 /**
  * Created by Alex on 22/04/2015.
  */
-public class MeasuresScreen extends FragmentActivity {
+public class MeasuresScreen extends Activity {
     private OpenMrsApi openMrsApi;
-    ViewPager viewpager;
-    ActionBar tabs;
-    FragmentPagerAdapter fragmentPager;
-
+    private ProgressDialog startGLDialog;
+    private String[] obs;
+    private TextView t;
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-        String[] obs = openMrsApi.getObs();
-
         setContentView(R.layout.measuresscreen);
-        viewpager = (ViewPager) findViewById(R.id.view_pager);
+        t=(TextView)findViewById(R.id.textView18);
+        obs=null;
 
-        fragmentPager = new measureAdapter(getSupportFragmentManager());
+        showDialog(0);
 
-
-        Bundle args = new Bundle();
-        args.putStringArrayList("measures", null);
-
-        viewpager.setOnPageChangeListener(
-                new ViewPager.SimpleOnPageChangeListener() {
-                    @Override
-                    public void onPageSelected(int position) {
-
-                        tabs = getActionBar();
-                        tabs.setSelectedNavigationItem(position);
-                    }
-                });
-        viewpager.setAdapter(fragmentPager);
-
-        tabs = getActionBar();
-        tabs.setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
-        ActionBar.TabListener tabListener = new ActionBar.TabListener() {
-
+        new Thread(new Runnable() {
             @Override
-            public void onTabSelected(ActionBar.Tab tab, FragmentTransaction ft) {
+            public void run() {
+                try {
+                   obs=new downloadObsAsync().executeOnExecutor(AsyncTask.THREAD_POOL_EXECUTOR ).get();
+                    startGLDialog.dismiss();
+                    String[] column = { "#Row","Observation"};
+                    int rl=obs.length; int cl=column.length;
+                    final ScrollView sv = new ScrollView(getApplicationContext());
+                    TableLayout tableLayout = createTableLayout(obs, column,rl, cl);
+
+                    HorizontalScrollView hsv = new HorizontalScrollView(getApplicationContext());
+                    hsv.addView(tableLayout);
+
+                    sv.addView(hsv);
+                    t.post(new Runnable() {
+                         @Override
+                         public void run() {
+                            MeasuresScreen.this.setContentView(sv);
+                         }
+                     });
 
 
-                viewpager.setCurrentItem(tab.getPosition());
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                } catch (ExecutionException e) {
+                    e.printStackTrace();
+                }
+                catch (Exception e){
+                    Log.i("aaa", "error: " + e.getMessage());
+                }
+
             }
+        }).start();
 
-            @Override
-            public void onTabUnselected(ActionBar.Tab tab, FragmentTransaction ft) {
 
-            }
-
-            @Override
-            public void onTabReselected(ActionBar.Tab tab, FragmentTransaction ft) {
-
-            }
-        };
-        /////////////////////////////
-        tabs.addTab(tabs.newTab().setText("Blood Glucose").setTabListener(tabListener));
-        tabs.addTab(tabs.newTab().setText("Ketanuria").setTabListener(tabListener));
-        tabs.addTab(tabs.newTab().setText("Blood Pressure").setTabListener(tabListener));
 
     }
 
 
+    @Override
+    protected Dialog onCreateDialog(int id) {
 
+        startGLDialog = new ProgressDialog(this);
+        // Set Dialog message
+        startGLDialog.setMessage("Please Wait for Collecting Data..");
+        startGLDialog.setTitle("Getting Patient Data...");
 
+        // Dialog will be displayed for an unknown amount of time
+        startGLDialog.setIndeterminate(true);
+        startGLDialog.setProgressStyle(ProgressDialog.STYLE_SPINNER);
+        startGLDialog.setCancelable(false);
+        startGLDialog.show();
+        return startGLDialog;
 
-    /* make original table
-    ===============================
-    private void ttmep()
-    {
-        String[] column = { "#Row","Observation"};
-        int rl=obs.length; int cl=column.length;
-
-//        Log.d("--", "R-Lenght--"+rl+"   "+"C-Lenght--"+cl);
-
-        ScrollView sv = new ScrollView(this);
-        TableLayout tableLayout = createTableLayout(obs, column,rl, cl);
-        HorizontalScrollView hsv = new HorizontalScrollView(this);
-        hsv.addView(tableLayout);
-        sv.addView(hsv);
-        setContentView(sv);
     }
+private class downloadObsAsync extends AsyncTask<String, Void, String[]> {
 
+
+    @Override
+    protected String[] doInBackground(String... params) {
+        String baseUrl = new ConfigReader(getApplicationContext()).getProperties().getProperty("openMRS_URL");
+        openMrsApi = new OpenMrsApi(baseUrl);
+        String [] ans = openMrsApi.getObs();
+
+        return ans;
+    }
+}
     @TargetApi(Build.VERSION_CODES.JELLY_BEAN)
     private TableLayout createTableLayout(String [] obs, String [] cv,int rowCount, int columnCount) {
         // 1) Create a tableLayout and its params
         TableLayout.LayoutParams tableLayoutParams = new TableLayout.LayoutParams();
-        TableLayout tableLayout = new TableLayout(this);
+        TableLayout tableLayout = new TableLayout(getApplicationContext());
         Drawable res = getResources().getDrawable(R.drawable.health_background);
         tableLayout.setBackground(res);
-        TextView messuresText = new TextView(this);
+        TextView messuresText = new TextView(getApplicationContext());
+
         messuresText.setText("measures");
-        messuresText.setTextSize(23);
+        messuresText.setTextSize(18);
         messuresText.setTextColor(Color.parseColor("#ff9cffd4"));
         tableLayout.addView(messuresText);
         // 2) create tableRow params
         TableRow.LayoutParams tableRowParams = new TableRow.LayoutParams();
-        tableRowParams.setMargins(1, 1,1, 1);
+        tableRowParams.setMargins(0, 0,0, 0);
         tableRowParams.weight = 1;
-
+        Drawable drawable= null;
         for (int i = 0; i < rowCount; i++) {
             // 3) create tableRow
             TableRow tableRow = new TableRow(this);
-            tableRow.setBackgroundColor(Color.BLACK);
             for (int j= 0; j < columnCount; j++) {
                 // 4) create textView
                 TextView textView = new TextView(this);
-                //  textView.setText(String.valueOf(j));
-                textView.setBackgroundColor(Color.WHITE);
+
+                textView.setPadding(5,0,0,0);
+                textView.setGravity(Gravity.CENTER);
+                TableLayout.LayoutParams p=new TableLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT);
+                p.weight=1;
+                 p.width=1;
+                textView.setLayoutParams(p);
+
 
                 if (i ==0){
                     textView.setText(cv[j]);
+                    drawable =  getResources().getDrawable(R.drawable.title_shape);
+                    textView.setBackground(drawable);
+                    textView.setWidth(0);
                 } else if(i>0 && j==0){
                     textView.setText("Row "+i);
+                    drawable =  getResources().getDrawable(R.drawable.title_shape);
+                    textView.setBackground(drawable);
                 }else {
+                    drawable =  getResources().getDrawable(R.drawable.cell_shape);
                     textView.setText(obs[i]);
+                    textView.setBackground(drawable);
 
                 }
-                textView.setGravity(Gravity.LEFT);
+
                 // 5) add textView to tableRow
                 tableRow.addView(textView, tableRowParams);
             }
@@ -174,9 +169,6 @@ public class MeasuresScreen extends FragmentActivity {
 
         return tableLayout;
     }
-
-    */
-
 }
 
 
